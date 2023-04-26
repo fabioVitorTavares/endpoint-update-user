84bcf3a0a32c4ed62c6bf0ba1ff7d11c1fac1abc
fabioVitorTavares <fabio.furtado@estudante.ufla.br>
2023-04-26 18:09:30 -0300

    commit

---
+++ src/test/java/com/endpointupdateuser/EndpointUpdateUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.endpointupdateuser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EndpointUpdateUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
